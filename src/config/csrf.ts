e29b8c9608c95068236c93b74e02be9fb70c2353
David Patrick Chuks <161928481+David-patrick-chuks@users.noreply.github.com>
2024-11-17 20:37:44 +0100

    node

--- src/config/csrf.ts
+++ src/config/csrf.ts
@@ -1,13 +1,17 @@
 import csrf from 'csurf';
 // const csrfProtection = csrf({ cookie: true });
 export const csrfProtection = csrf({
-  cookie: true,
-    // cookie: {
-    //   httpOnly: true, // Ensures the cookie is not accessible via JavaScript
-    //   secure: process.env.NODE_ENV === 'production', // Use secure cookies in production (HTTPS)
-    //   sameSite: 'strict', // Helps prevent CSRF attacks by allowing cookies only from the same site
-    //   maxAge: 60 * 60 * 24 * 7 // Cookie expiry in seconds (e.g., 7 days)
-    // },
-    // ignoreMethods: ['GET', 'HEAD', 'OPTIONS'], // Methods to ignore CSRF check
-  });
-  
+  // cookie: true,
+  cookie: {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
+    sameSite: 'lax', // or 'strict', adjust based on your frontend-backend interaction
+  },
+  // cookie: {
+  //   httpOnly: true, // Ensures the cookie is not accessible via JavaScript
+  //   secure: process.env.NODE_ENV === 'production', // Use secure cookies in production (HTTPS)
+  //   sameSite: 'strict', // Helps prevent CSRF attacks by allowing cookies only from the same site
+  //   maxAge: 60 * 60 * 24 * 7 // Cookie expiry in seconds (e.g., 7 days)
+  // },
+  // ignoreMethods: ['GET', 'HEAD', 'OPTIONS'], // Methods to ignore CSRF check
+});
