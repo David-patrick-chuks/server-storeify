18e339b7b7002506ebfc1c0793894bffa157aaea
David Patrick Chuks <161928481+David-patrick-chuks@users.noreply.github.com>
2024-11-19 11:25:27 +0100

    last update

--- src/routes/notifyRoute.js
+++ src/routes/notifyRoute.js
@@ -1,10 +1,11 @@
 import {Router} from "express"
 import { notify , getNotifyById} from "../controllers/notifications.js";
+import { authenticateJWT } from "../utils/authMiddleware.js";
  
 const router = Router();
 
 // Route to fetch all notifications for a client
-router.get('/notify', notify);
-router.get('/notify/:projectId', getNotifyById);
+router.get('/notify', authenticateJWT, notify);
+router.get('/notify/:projectId',authenticateJWT, getNotifyById);
 
 export default router;
