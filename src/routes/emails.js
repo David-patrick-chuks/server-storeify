c924c074c64456ff132d1709d5accb00494fa428
David Patrick Chuks <161928481+David-patrick-chuks@users.noreply.github.com>
2024-11-19 07:42:29 +0100

    update

--- src/routes/emails.js
+++ src/routes/emails.js
@@ -7,13 +7,13 @@ const router = express.Router();
 
 // Protect routes with authenticateJWT and isAuthorized middleware
 
-router.get('/client-email-list',  getEmailList);
-router.get('/emails/total',  getEmailsTotal);
-router.post('/reply/:messageId',  replyToEmail);
-router.get('/emails',  getEmailsWithPagination);
-router.get('/emails/:messageId',  getEmailByMessageId);
-router.post('/send',  sendEmailController);
-router.post('/send-bulk',  sendBulkEmailController); // New route for bulk emails
-router.delete('/delete/:id',  deleteEmail);
+router.get('/client-email-list', authenticateJWT, getEmailList);
+router.get('/emails/total', authenticateJWT, getEmailsTotal);
+router.post('/reply/:messageId', authenticateJWT, replyToEmail);
+router.get('/emails', authenticateJWT, getEmailsWithPagination);
+router.get('/emails/:messageId', authenticateJWT, getEmailByMessageId);
+router.post('/send', authenticateJWT, isAuthorized, sendEmailController);
+router.post('/send-bulk', authenticateJWT, isAuthorized, sendBulkEmailController); // New route for bulk emails
+router.delete('/delete/:id', authenticateJWT, isAuthorized, deleteEmail);
 
 export default router;
