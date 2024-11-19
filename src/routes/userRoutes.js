f29f32754a66aca77838e2f34e936d30f9522ee0
David Patrick Chuks <161928481+David-patrick-chuks@users.noreply.github.com>
2024-11-19 17:39:08 +0100

    bebeb

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,22 +1,24 @@
-import { Router } from 'express';
-import { getUserProfile, updateIsNotify, checkAuthentication, updatePassword } from '../controllers/userController.js';
-import { authenticateJWT } from '../utils/authMiddleware.js';
+import { Router } from "express";
+import {
+  getUserProfile,
+  updateIsNotify,
+  checkAuthentication,
+  updatePassword,
+} from "../controllers/userController.js";
+import { authenticateJWT } from "../utils/authMiddleware.js";
 
 const router = Router();
 
 // Route to get user profile
-router.get('/profile', getUserProfile);
+router.get("/profile", authenticateJWT, getUserProfile);
 
-router.put('/email-notify', updateIsNotify);
+router.put("/email-notify", updateIsNotify);
 
-router.put('/update-password', updatePassword);
-
+router.put("/update-password", updatePassword);
 
 // Route to update user profilep
 // router.put('/profile', authenticateJWT, updateUserProfile);
 
-
-router.get('/check-auth', checkAuthentication);
-
+router.get("/check-auth", authenticateJWT, checkAuthentication);
 
 export default router;
