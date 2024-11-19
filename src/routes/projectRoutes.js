e552763e61a4426f9b5e0f5b3afadff43b440dc5
David Patrick Chuks <161928481+David-patrick-chuks@users.noreply.github.com>
2024-11-19 11:23:54 +0100

    Last Update

--- src/routes/projectRoutes.js
+++ src/routes/projectRoutes.js
@@ -1,28 +1,35 @@
-import express from 'express';
-import { createProject,getCombinedAnalytics, getAllProjects, getProjectById, updateProject, deleteProject, deleteAllProjects } from '../controllers/projectController.js';
-import { authenticateJWT, isAuthorized } from '../utils/authMiddleware.js';
-
+import express from "express";
+import {
+  createProject,
+  getCombinedAnalytics,
+  getAllProjects,
+  getProjectById,
+  updateProject,
+  deleteProject,
+  deleteAllProjects,
+} from "../controllers/projectController.js";
+import { authenticateJWT, isAuthorized } from "../utils/authMiddleware.js";
 
 const router = express.Router();
 
 // Route to create a new project
-router.post('/create-project', createProject);
+router.post("/create-project", authenticateJWT, createProject);
 
 // Route to get all projects
-router.get('/all', getAllProjects);
+router.get("/all", authenticateJWT, getAllProjects);
 
-router.get('/analytics', getCombinedAnalytics);
+router.get("/analytics", authenticateJWT, getCombinedAnalytics);
 
 // Route to get a single project by ID
-router.get('/:id', getProjectById);
+router.get("/:id", authenticateJWT, getProjectById);
 
 // Route to update a project by ID
-router.put('/:id', updateProject);
+router.put("/:id", authenticateJWT, updateProject);
 
 // Route to delete a single project by ID
-router.delete('/:id', deleteProject);
+router.delete("/:id", authenticateJWT, deleteProject);
 
 // Route to delete all projects
-router.delete('/all', deleteAllProjects);
+router.delete("/all", authenticateJWT, deleteAllProjects);
 
 export default router;
