5baeaaa883c5bd6583bf6903dc6fd3403f64f4ce
David Patrick Chuks <161928481+David-patrick-chuks@users.noreply.github.com>
2024-11-20 17:49:58 +0100

    lastttt commmit

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,6 +1,6 @@
 // CORS configuration
 export const corsOptions = {
-    origin: process.env.CORS_ORIGIN || "https://storeify-management.vercel.app", 
+    origin: [process.env.CORS_ORIGIN || "https://storeify-management.vercel.app", "https://admin.storeify.co"], 
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "CSRF-Token", "Authorization"],
     credentials: true,
